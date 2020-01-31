1e8bb476d61c9ed725c08b084ef4ae0c293bd55a
Pollen <60172975+PollenZer@users.noreply.github.com>
2020-01-31 11:55:21 +0100

    jolie navBar + joli Titre + color Picker en cours

--- src/App.js
+++ src/App.js
@@ -6,29 +6,55 @@ import Datas from './Navigation/Datas/Page.js'
 import Home from './Navigation/Home/Page.js'
 import Users from './Navigation/Users/Page.js'
 import FourOFour from './Navigation/FourOFour/Page.js'
-import Test from './test'
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import MenuIcon from '@material-ui/icons/Menu';
-import red from '@material-ui/core/colors/red';
+import SettingsIcon from '@material-ui/icons/ColorLens';
+import yellow from '@material-ui/core/colors/yellow';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import './Fonts/font.css'
 
-const primary = red[500]; // #F44336
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: localStorage.getItem("color") || yellow[500],
+      // main: yellow[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
 
 const style={
   itemMenuList:{
     width:"100%",
-    height:"5vh",
+    height:"7vh",
     textDecoration:"none",
-    fontFamily:"Roboto",
+    // fontFamily:"Roboto",
     color:"black",
+    verticalAlign:'middle',
+    lineHeight:"7vh",
+    textAlign:"center",
+    fontFamily:"RobotoR",
   },
   backgroundLinear:{
     background: "linear-gradient(to left, red, blue)",
+  },
+  closeDrawerButton:{
+    margin:'9px'
+  },
+  titreDeLaPage:{
+    margin:'auto auto',
   },
 }
 
@@ -48,62 +74,82 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-      <Drawer 
-        open={this.state.left} 
-        onClose={this.toggleDrawer(false)}
-        >
-          <div
-          style={{width: "35vw"}}
-          onClick={this.toggleDrawer(false)}
-          onKeyDown={this.toggleDrawer(false)}
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>
+        <Drawer 
+          open={this.state.left} 
+          onClose={this.toggleDrawer(false)}
           >
-            <List>
-              <ListItem button >
-                <Link to="/home" style={style.itemMenuList} >Home</Link>
-              </ListItem>
-              <ListItem button >
-                <Link to="/users" style={style.itemMenuList} >Users</Link>
-              </ListItem>
-              <ListItem button >
-                <Link to="/datas" style={style.itemMenuList}>Datas</Link>
-              </ListItem>
-              <ListItem button >
-                <Link to="/contact" style={style.itemMenuList}>Contact</Link>
-              </ListItem>
-            </List>
-          </div>
-          </Drawer>
-      
-          <AppBar position="static" color="secondary">
-            <Toolbar>
-              <IconButton 
+            <div
+            style={{width: "35vw"}}
+            onClick={this.toggleDrawer(false)}
+            onKeyDown={this.toggleDrawer(false)}
+            >
+              <IconButton
+                onClick={this.toggleDrawer(false)}
                 edge="start" 
                 color="inherit" 
+                style={style.closeDrawerButton}
                 aria-label="menu"
                 >
-                <MenuIcon onClick={this.toggleDrawer(true)}/>
+                <CloseIcon />
               </IconButton>
-              <Typography 
-                variant="h6" 
-                // className={classes.title}
-                >
-                News
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Switch>
-            <Route path="/contact" component={Contact} />
-            <Route path="/datas" component={Datas} />
-            <Route path="/users" component={Users} />
-            <Route path="/test" component={Test} />
-            <Route path="/home" component={Home} />
-            <Route path="/" component={Home} />
-            <Route path="*" component={FourOFour} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+              <Divider />
+              <List>
+                <ListItem button >
+                  <Link to="/home" style={style.itemMenuList} >Home</Link>
+                </ListItem>
+                <ListItem button >
+                  <Link to="/users" style={style.itemMenuList} >Users</Link>
+                </ListItem>
+                <ListItem button >
+                  <Link to="/datas" style={style.itemMenuList}>Datas</Link>
+                </ListItem>
+                <ListItem button >
+                  <Link to="/contact" style={style.itemMenuList}>Contact</Link>
+                </ListItem>
+              </List>
+            </div>
+            </Drawer>
+        
+            <AppBar position="static" color="primary">
+              <Toolbar>
+                  <IconButton
+                    onClick={this.toggleDrawer(true)}
+                    edge="start" 
+                    color="inherit" 
+                    aria-label="menu"
+                    >
+                    <MenuIcon/>
+                </IconButton>
+                <Typography 
+                  variant="h6" 
+                  style={style.titreDeLaPage}
+                  >
+                  Pollinarium
+                </Typography>
+                <IconButton
+                  onClick={this.toggleDrawer(true)}
+                  edge="end" 
+                  color="inherit" 
+                  aria-label="menu"
+                  >
+                  <SettingsIcon/>
+                </IconButton>
+              </Toolbar>
+            </AppBar>
+            <Switch>
+              <Route path="/contact" component={Contact} />
+              <Route path="/datas" component={Datas} />
+              <Route path="/users" component={Users} />
+              <Route path="/home" component={Home} />
+              <Route path="/" component={Home} />
+              <Route path="*" component={FourOFour} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
     );
   }
 }
